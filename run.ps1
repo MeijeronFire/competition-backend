d9eca7b883a6efe981ed15aef84adcae55638174
Otto Crawford <cobalt_canon@protonmail.ch>
2026-05-07 17:45:34 +0200

    fixed powershell

--- run.ps1
+++ run.ps1
@@ -2,9 +2,9 @@
 # Copyright (C) 2026 Otto Crawford
 
 Write-Output "Competition backend - Copyright (C) 2026 Otto Crawford "
-Write-Output "This program comes with ABSOLUTELY NO WARRANTY; " \
-      "This is free software, and you are welcome to redistribute it" \
-      "under certain conditions;" \
+Write-Output "This program comes with ABSOLUTELY NO WARRANTY; " `
+      "This is free software, and you are welcome to redistribute it" `
+      "under certain conditions;"
 
 python -m uvicorn app:app `
   --host 0.0.0.0 `
