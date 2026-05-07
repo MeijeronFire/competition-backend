b9bea77d1cee87d0f724426629d34970cb768e8e
Otto Crawford <cobalt_canon@protonmail.ch>
2026-05-07 17:48:23 +0200

    actual for real

--- run.ps1
+++ run.ps1
@@ -6,7 +6,7 @@ Write-Output "This program comes with ABSOLUTELY NO WARRANTY; " `
       "This is free software, and you are welcome to redistribute it" `
       "under certain conditions;"
 
-.\.venv\bin\python -m uvicorn app:app `
+.\.venv\Scripts\python -m uvicorn app:app `
   --host 0.0.0.0 `
   --port 8000 `
   --ssl-keyfile key.pem `
